ffae004ed616685b8b0b78bc8e9682976f2c6603
Kate <petl@bk.ru>
2022-10-16 09:46:42 +0400

    delete

--- Program.cs
+++ Program.cs
@@ -1,20 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// Example a = 5; b = 7 -> max = 7
 
-Console.WriteLine("vvedite  chislo");
-int input=int.Parse(Console.ReadLine()!);
-Console.WriteLine("vvedite drugoe chislo");
-int input2=int.Parse(Console.ReadLine()!);
-if (input>input2) 
-{
-    Console.WriteLine($"Max {input} Min {input2}");  
-}
-else
-{
-    Console.WriteLine($"Max {input2} Min {input}");
-}
-    
+
     
     
     
